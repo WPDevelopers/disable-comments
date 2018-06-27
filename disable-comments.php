b3aef72e9ab4c1c209af80700775f80591662d2e
Garrett Hyder <garrett@eclipse3sixty.com>
2018-06-26 22:00:00 -0700

    Add discussion_settings_allowed settings
    Initial setup to address #48

--- disable-comments.php
+++ disable-comments.php
@@ -325,11 +325,13 @@ jQuery(document).ready(function($){
 	public function filter_admin_menu(){
 		global $pagenow;
 
-		if ( $pagenow == 'comment.php' || $pagenow == 'edit-comments.php' || $pagenow == 'options-discussion.php' )
+		if ( $pagenow == 'comment.php' || $pagenow == 'edit-comments.php' || ( ! $this->discussion_settings_allowed() && $pagenow == 'options-discussion.php' ) )
 			wp_die( __( 'Comments are closed.' ), '', array( 'response' => 403 ) );
 
 		remove_menu_page( 'edit-comments.php' );
-		remove_submenu_page( 'options-general.php', 'options-discussion.php' );
+		if ( ! $this->discussion_settings_allowed() ) {
+			remove_submenu_page( 'options-general.php', 'options-discussion.php' );
+		}
 	}
 
 	public function filter_dashboard(){
@@ -449,6 +451,12 @@ jQuery(document).ready(function($){
 		}
 	}
 
+	private function discussion_settings_allowed() {
+		if( defined( 'DISABLE_COMMENTS_ALLOW_DISCUSSION_SETTINGS' ) && DISABLE_COMMENTS_ALLOW_DISCUSSION_SETTINGS == false ) {
+			return false;
+		}
+	}
+	
 	public function single_site_deactivate() {
 		// for single sites, delete the options upon deactivation, not uninstall
 		delete_option( 'disable_comments_options' );
