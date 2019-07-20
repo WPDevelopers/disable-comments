3672cd5872dc7b3a23ca1c2ee40b709fccf046e4
Samir Shah <solaris.smoke@gmail.com>
2019-07-20 16:06:47 +0300

    Version 1.10.1

--- disable-comments.php
+++ disable-comments.php
@@ -3,7 +3,7 @@
 Plugin Name: Disable Comments
 Plugin URI: https://wordpress.org/plugins/disable-comments/
 Description: Allows administrators to globally disable comments on their site. Comments can be disabled according to post type.
-Version: 1.10.0
+Version: 1.10.1
 Author: Samir Shah
 Author URI: http://www.rayofsolaris.net/
 License: GPL2
