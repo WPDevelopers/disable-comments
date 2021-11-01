5a8098b6b4a8b4090793a60ebd0d3296c2694fcc
WPDev-Mac-Rojab <irojabkhan@gmail.com>
2021-11-01 12:36:32 +0600

    dropdown-added

--- views/partials/_sites.php
+++ views/partials/_sites.php
@@ -1,4 +1,6 @@
-<div class="has-pagination">
+
+
+<div class="">
 <div class="d__flex mb15 space__between">
     <div class='subsite__checklist__item' style="flex: 1 1 200px;">
         <input
@@ -13,15 +15,32 @@
             <small>(0 selected)</small>
         </label>
     </div>
-    <div class="icon__input" style="text-align: right; flex: 0 0 200px;">
-        <span class="icon">
-            <img src="<?php echo esc_url(DC_ASSETS_URI . 'img/search.svg'); ?>" alt="">
-        </span>
-        <input type="text" class="form__control w-100" placeholder="Search...">
+    <div class="mb10" style="text-align: right; flex: 0 0 200px;">
+        <div class="icon__input">
+            <span class="icon">
+                <img src="<?php echo esc_url(DC_ASSETS_URI . 'img/search.svg'); ?>" alt="">
+            </span>
+            <input type="text" class="form__control w-100" placeholder="Search...">
+        </div>
     </div>
 </div>
 <div class="sites_list">
 
+</div>
+<div class="d__flex space__between">
+    <div class="d__flex item__number__controller">
+        <p>Show Items:</p>
+        <div class="dc-select">
+            <span class="icon"></span>
+            <select class="form__control">
+                <option value="">10</option>
+                <option value="">20</option>
+                <option value="">30</option>
+                <option value="">200</option>
+            </select>
+        </div>
+    </div>
+    <div class="has-pagination"></div>
 </div>
 </div>
 
